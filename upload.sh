d52e6b426dd778cdc739331dd873a0aece92f6fc
Shaun Mouton <shaun.mouton@kasasa.com>
2016-03-22 17:23:30 -0500

    fix upload ffs

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync . s3://tirefi.re/ --exclude 'LICENSE' --exclude 'README.md' --exclude '*.sh' --exclude '.git'
+aws s3 sync . s3://tirefi.re/ --exclude '*' --include '*.html' --include '*.gif' --include '*.jpeg'
